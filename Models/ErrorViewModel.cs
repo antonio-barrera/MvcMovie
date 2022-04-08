f10a2069894f710b2c9647c01eb0b0e113035bf3
Antonio Barrera <jeantonio.barrera@gmail.com>
2022-04-08 00:15:06 -0600

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+#nullable enable
+namespace MvcMovie.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
